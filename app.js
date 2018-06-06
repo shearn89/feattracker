7a568d1c46c5c7be799642e0523d223bd707e26c
shearn89 <alexander.shearn@gmail.com>
2018-06-06 10:53:03 +0100

    adding recommended debugging tools

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+// Entrypoint to the app.
+// requires
 var express = require('express');
 var chalk = require('chalk');
+var debug = require('debug')('feattracker:app');
+var morgan = require('morgan');
 
+// app setup
 var app = express();
+app.use(morgan('tiny'));
 
+// routing
 var badge = require('./routes/badge');
 
 app.get('/', () => res.send('Hello World!'))
 app.get('/badges/:badgeId?*', badge.get);
 app.post('/badges/:badgeId?*', badge.post);
 
+// env vars
 var port = process.env.NODEJS_PORT || 3000;
 
-app.listen(port, () => console.log('Example app listening on port '+port))
+// run dis.
+app.listen(port, () => debug(`Example app listening on port ${chalk.green(port)}`));
--- routes/badge.js
+++ routes/badge.js
@@ -1,5 +1,7 @@
 // badge.js
 // ========
+var debug = require('debug')('feattracker:routes:badge');
+
 module.exports = {
   get: function (req, res, next) {
     process(req, res, next, 'get');
@@ -13,10 +15,10 @@ var badges = [];
 
 var process = function (req, res, next, method) {
   if (!req.params.badgeId) {
-    console.log(method + '/badges');
+    debug(method + '/badges');
     res.json(badges);
   } else {
-    console.log(method + ' /badges/' + req.params.badgeId);
+    debug(method + ' /badges/' + req.params.badgeId);
     res.send(method + ' /badges/' + req.params.badgeId);
   }
   next();
