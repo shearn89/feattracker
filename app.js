5ca7d7ee747a8e6cceeb14862c9e495d611e5b97
Alex Shearn <alexander.shearn@gmail.com>
2018-06-24 14:55:18 +1200

    Adding EJS template and Bootstrap layout

--- app.js
+++ app.js
@@ -20,16 +20,19 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 // routing
 const badge = require('./routes/badge');
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, viewsDir, 'index.html')));
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, viewsDir, 'index.html')));
+app.get('/', (req, res) => res.render('index', { title: 'FeatTracker', list: ['a', 'b'] }));
 app.get('/badges/:badgeId?*', badge.get);
 app.post('/badges/:badgeId?*', badge.post);
 
 // env vars
-const port = process.env.NODEJS_PORT || 3000;
+const port = process.env.NODE_PORT || 3000;
 
 // run dis.
-app.listen(port, () => debug(`Example app listening on port ${chalk.green(port)}`));
+app.listen(port, () => debug(`App listening on port ${chalk.green(port)}`));
